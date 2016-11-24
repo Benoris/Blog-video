d203451723627cf6a58f42562d9296d89a8a1293
Benoris <maurice.dinh@gmail.com>
2016-11-24 11:06:34 +0100

    video controller

--- startbootstrap-1-col-portfolio-gh-pages/controllers/addvideo.php
+++ startbootstrap-1-col-portfolio-gh-pages/controllers/addvideo.php
@@ -1,3 +1,50 @@
 <?php
 
+require_once '../model/video.php';
 
+addVideo($title, $soustitre, $description, $link, $mdp, $categorie);
+
+header("Location:listvideo.php");
+
+
+$erreurs = array();
+
+if (filter_has_var(INPUT_POST,'submit')) {
+     // récupération des données provenant des données saisies par l'utilisateur
+    
+    $idVideo = filter_input(INPUT_POST, 'idVideo',FILTER_VALIDATE_INT);
+    $title = trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
+    $soustitre = trim(filter_input(INPUT_POST,'soustitre',FILTER_SANITIZE_STRING));
+    $description = trim(filter_input(INPUT_POST,'description',FILTER_SANITIZE_STRING));
+    $link = trim(filter_input(INPUT_POST,'link',FILTER_SANITIZE_STRING));
+    $mdp = trim(filter_input(INPUT_POST,'pwd',FILTER_SANITIZE_STRING));
+    $categorie = filter_input(INPUT_POST, 'categorie',FILTER_VALIDATE_INT);
+                                              // vérification des données saisies
+    if (empty($LastName))
+        $errors['LastName'] = "Le nom ne peut pas être vide";
+    if (empty($FirstName))
+        $errors['FirstName'] = "Le prénom ne peut pas être vide";
+    if (empty($Pseudo))
+        $errors['Pseudo'] = "Le pseudo ne peut pas être vide";
+    if (empty($Pwd))
+        $errors['Pwd'] = "Le mot de passe ne peut pas être vide";
+    if ($Pwd != $Pwd2)
+        $errors['Pwd2'] = "Les deux mots de passes sont différents";
+    
+                            // si il n'y a pas d'erreur dans les données saisies
+    if (empty($errors)) {
+
+            require_once 'model/users.php';
+            if (is_numeric($idUser)) {
+                updateUser($idUser,$LastName, $FirstName, $Pseudo, $Pwd);
+                SetMessageFlash("Utilisateur modifié ($FirstName $LastName / $Pseudo)");        
+            } else {
+            $idUser = addUser($LastName, $FirstName, $Pseudo, $Pwd);
+            SetMessageFlash("Utilisateur ajouté ($FirstName $LastName / $Pseudo)");
+            }
+            header("location:listusers.html");
+            exit;
+    }
+}
+
+include 'views/userform.php';
--- startbootstrap-1-col-portfolio-gh-pages/index.php
+++ startbootstrap-1-col-portfolio-gh-pages/index.php
@@ -2,34 +2,16 @@
 
 $action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_URL);
 if (empty($action)) {
-    $action = 'listusers';
+    $action = 'listvideo';
 }
 
 try {
     switch ($action) {
-        case 'listusers':
-            require_once 'controllers/listusers.php';
+        case 'listvideo':
+            require_once 'controllers/listvideo.php';
             break;
-        case 'edituser':
-            require_once 'controllers/edituser.php';
-            break;
-        case 'saveuser':
-            require_once 'controllers/saveuser.php';
-            break;
-        case 'deleteuser':
-            require_once 'controllers/deleteuser.php';
-            break;
-        case 'listevents':
-            require_once 'controllers/listevents.php';
-            break;
-        case 'editevent':
-            require_once 'controllers/editevent.php';
-            break;
-        case 'saveevent':
-            require_once 'controllers/saveevent.php';
-            break;
-        case 'deleteevent':
-            require_once 'controllers/deleteevent.php';
+        case 'addvideo':
+            require_once 'controllers/addvideo.php';
             break;
         default:
             require_once 'views/404.php';
