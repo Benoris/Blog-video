7bdb18d20cc3ea6c3539cd2875a338ba78a8291e
Benoris <maurice.dinh@gmail.com>
2016-11-24 09:38:20 +0100

    function addVideo

--- startbootstrap-1-col-portfolio-gh-pages/model/video.php
+++ startbootstrap-1-col-portfolio-gh-pages/model/video.php
@@ -9,3 +9,23 @@ function getVideo()
     return $query->fetchAll();
 }
 
+require_once 'dbconnection.php';
+
+function addVideo($title, $soustitre, $description, $link, $mdp, $categorie) {
+    $db = connectDb();
+    $sql = "INSERT INTO video(Titre,SousTitre,Description,Lien,MDP,idCategorie) " .
+            " VALUES (:Titre, :SousTitre, :Description, :Lien, :MDP, :idCategorie)";
+    $request = $db->prepare($sql);
+    if ($request->execute(array(
+                'Titre' => $title,
+                'SousTitre' => $soustitre,
+                'Description' => $description,
+                'Lien' => $link,
+                'MDP' => sha1($mdp),
+                'idCategorie' => $categorie
+            ))) {
+        return $db->lastInsertID();
+    } else {
+        return NULL;
+    }
+}
