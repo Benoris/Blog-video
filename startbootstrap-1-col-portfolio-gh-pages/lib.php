9f1fb99127e2ecfab6b039481b38de141685ac72
psccc97 <pascucci.lino@gmail.com>
2016-11-24 08:31:00 +0100

    ajout de la fonction connectDb()

---
+++ startbootstrap-1-col-portfolio-gh-pages/lib.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * fonction qui permet de se connecter à la bd
+ * @return \PDO
+ */
+function connectDb ()
+{
+    $host = "localhost";
+    $dbname = "db_blog_video";
+    $user = "root";
+    $password = "";
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;    
+    $bd = new PDO ('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$user,$password,$pdo_options);
+    return $bd;
+}
