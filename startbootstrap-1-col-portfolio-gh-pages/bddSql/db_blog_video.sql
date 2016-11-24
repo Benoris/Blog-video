75abf4c47de047cf44d19676b368640c70f425e1
psccc97 <pascucci.lino@gmail.com>
2016-11-24 09:09:34 +0100

    sql BDD

---
+++ startbootstrap-1-col-portfolio-gh-pages/bddSql/db_blog_video.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 24 Novembre 2016 à 09:08
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `db_blog_video`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `NomCategorie` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `categorie`
+--
+
+INSERT INTO `categorie` (`idCategorie`, `NomCategorie`) VALUES
+(1, 'Action'),
+(2, 'Comédie'),
+(3, 'Drame'),
+(4, 'Thriller');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `idVideo` int(11) NOT NULL AUTO_INCREMENT,
+  `Titre` varchar(50) NOT NULL,
+  `SousTitre` varchar(50) DEFAULT NULL,
+  `Description` varchar(50) NOT NULL,
+  `Lien` varchar(500) NOT NULL,
+  `MDP` varchar(50) DEFAULT NULL,
+  `idCategorie` int(11) NOT NULL,
+  PRIMARY KEY (`idVideo`),
+  KEY `FK_video_idCategorie` (`idCategorie`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `video`
+--
+
+INSERT INTO `video` (`idVideo`, `Titre`, `SousTitre`, `Description`, `Lien`, `MDP`, `idCategorie`) VALUES
+(5, 'Instrument génial', 'Wintergatan', 'Marble Machine (music instrument using 2000 marble', 'https://www.youtube.com/embed/IvUU8joBb1Q', NULL, 2),
+(6, 'Point Point - All This', 'Trap Natoin', 'musique de trap', 'https://www.youtube.com/embed/LMjIvK6ndSc', 'f6889fc97e14b42dec11a8c183ea791c5465b658', 4);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `video`
+--
+ALTER TABLE `video`
+  ADD CONSTRAINT `FK_video_idCategorie` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`idCategorie`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
